302492ecd90b5f9758a78e6f72f3ff2e26891f89
Marek Kurdej <marek@quasardb.net>
2016-09-16 11:46:39 +0200

    Add iostream benchmark.

--- bench/scanf.cpp
+++ bench/scanf.cpp
@@ -1,17 +1,33 @@
 #include <benchmark/benchmark.h>
 #include <cstdio>
+#include <sstream>
 #include <string>
 
+static const std::string input = "12 132.43 234.5 -43 -34e-3 -4.53e3";
+
 static void BM_scanf_numbers(benchmark::State& state) {
-    static const std::string s = "12 132.43 234.5 -43 -34e-3 -4.53e3";
     int i1, i2;
     float f1, f2;
     double d1, d2;
     while(state.KeepRunning()) {
-        std::sscanf(s.c_str(), "%d%f%lf%d%f%lf", &i1, &f1, &d1, &i2, &f2, &d2);
+        std::sscanf(input.c_str(), "%d%f%lf%d%f%lf", &i1, &f1, &d1, &i2, &f2, &d2);
     }
 }
 
 BENCHMARK(BM_scanf_numbers);
+
+static void BM_stringstream_numbers(benchmark::State& state) {
+    std::istringstream is(input);
+
+    int i1, i2;
+    float f1, f2;
+    double d1, d2;
+    while(state.KeepRunning()) {
+        is.seekg(0); // rewind
+        is >> i1 >> f1 >> d1 >> i2 >> f2 >> d2;
+    }
+}
+
+BENCHMARK(BM_stringstream_numbers);
 
 BENCHMARK_MAIN()
